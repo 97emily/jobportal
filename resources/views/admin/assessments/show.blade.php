1e03c50f97c28b79c805024f7ab76a75f6246014
henrymanyonyi <henrymanyonyi4@gmail.com>
2024-06-15 16:58:26 +0300

    assesements added to the code base

--- resources/views/admin/assessments/show.blade.php
+++ resources/views/admin/assessments/show.blade.php
@@ -10,45 +10,26 @@
                         <h1 class="product-title">{{ $assessment->title }}</h1>
                         <div class="product-price mb-2">
                             <!-- Display salary range if available -->
-                            @if ($assessment->salary_min && $assessment->salary_max)
-                                <span class="price text-highlight fs-lg fw-700">KES.
-                                    {{ number_format($assessment->salary_min, 0) }} -
-                                    KES. {{ number_format($assessment->salary_max, 0) }}</span>
+                            @if ($assessment->pass_mark)
+                                <span
+                                    class="price text-highlight fs-lg fw-700">{{ number_format($assessment->pass_mark, 0) }}</span>
                             @else
-                                <span class="price text-highlight fs-lg fw-700">Salary: Not specified</span>
+                                <span class="price text-highlight fs-lg fw-700">Passmark: Not specified</span>
                             @endif
                         </div>
                         <!-- Display assessment description -->
                         <div class="product-desc mt-2">
-                            <p>{!! $assessment->assessment_description !!}</p>
+                            <p>{!! $assessment->description !!}</p>
                         </div>
                     </div>
                     <!-- end product-detail -->
                     <hr>
                     <ul class="product-meta list-unstyled">
-                        {{-- <li>Category:
-                            @foreach ($assessment->categories as $category)
-                                <span class="badge bg-info">{{ $category->name }}</span>
-                            @endforeach
-                        </li> --}}
-                        <li>Tags:
-                            <span class="badge bg-success">{{ $assessment->tag->name }}</span>
+                        <li>Category:
+                            <span class="badge bg-success">{{ $assessment->category->name }}</span>
                         </li>
                     </ul>
                     <hr>
-                    <div class="row">
-                        <!-- Display additional assessment details such as available stock, number of orders, and revenue -->
-                        <div class="col-md-4">
-                            <h5 class="fw-700">Work Location:</h5>
-                            <p class="lh-150 text-sm">{{ $assessment->location ?: 'Not specified' }}</p>
-                        </div>
-                        <div class="col-md-4">
-                            <h5 class="fw-700">Closing Date:</h5>
-                            <p class="lh-150 text-sm">
-                                {{ \Carbon\Carbon::parse($assessment->closing_date)->format('d-m-Y') ?: 'Not specified' }}</p>
-                        </div>
-                        <!-- You can add more assessment details here as needed -->
-                    </div>
                     <!-- end row -->
                     <div class="row">
                         <!-- Button to edit the assessment posting -->
